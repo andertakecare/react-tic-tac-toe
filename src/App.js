1b0ff204d004d5eb979aab91c41eac9bf2d7149d
andertakecare <20363451+andertakecare@users.noreply.github.com>
2023-06-26 11:44:56 +0900

    add function to jump to board state at any move number

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import { useState } from "react";
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory((prevHistory) => [...prevHistory, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    // setHistory((prevHistory) => [...prevHistory, nextSquares]);
     setXIsNext(xIsNext ? false : true);
   }
 
   function jumpTo(nextMove) {
-    //TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
